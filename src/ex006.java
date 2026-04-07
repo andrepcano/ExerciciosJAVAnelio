a8d623db69d0f737d615a60644eb391bee9d8ce9
AndrÃé <andrepcano@gmail.com>
2026-04-07 18:05:26 -0300

    feat: Exercícios até Estrutura de repetições

---
+++ src/ex006.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            double media = a / b;
+            System.out.println("A média é " + media);
+        }
+        sc.close();
+    }
+}
---
+++ src/ex008.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ex008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
